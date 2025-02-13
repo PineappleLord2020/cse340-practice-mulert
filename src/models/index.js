09c1c6bd68c2fe409a966acde772d8ef55dd1582
PineappleLord2020 <mul20002@byui.edu>
2025-02-13 13:50:39 -0700

    Finished the Edit feature

--- src/models/index.js
+++ src/models/index.js
@@ -24,5 +24,43 @@ const getGamesByClassification = async (classificationId) => {
         ;
         return await db.all(query, [classificationId]);
 };
+
+const getGameById = async (gameId) => {
+    const db = await dbPromise;
+    const query = `
+        SELECT game.*, classification.classification_name 
+        FROM game 
+        JOIN classification ON game.classification_id = classification.classification_id
+        WHERE game.game_id = ?;
+    `;
+    return await db.get(query, [gameId]);
+};
  
-export { getClassifications, getGamesByClassification, addNewGame };
+async function updateGame(gameId, name, description, classificationId, imagePath = '') {
+    const db = await dbPromise;
+ 
+    // If no image was uploaded, update basic game info
+    if (imagePath === '') {
+        const sql = `
+            UPDATE game 
+            SET game_name = ?, 
+                game_description = ?, 
+                classification_id = ?
+            WHERE game_id = ?
+        `;
+        return await db.run(sql, [name, description, classificationId, gameId]);
+    }
+ 
+    // If image was uploaded, update all info including image
+    const sql = `
+        UPDATE game 
+        SET game_name = ?, 
+            game_description = ?, 
+            classification_id = ?,
+            image_path = ?
+        WHERE game_id = ?
+    `;
+    return await db.run(sql, [name, description, classificationId, imagePath, gameId]);
+}
+ 
+export { getClassifications, getGamesByClassification, addNewGame, getGameById, updateGame };
