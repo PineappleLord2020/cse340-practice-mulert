e6fc99c7ceaa248ddfaef0d294e7d2a639dcd2c5
PineappleLord2020 <mul20002@byui.edu>
2025-02-26 16:53:10 -0700

    Some more fixes

--- src/routes/account/index.js
+++ src/routes/account/index.js
@@ -14,20 +14,31 @@ router.post('/register', async(req, res) => {
     const confirmPW = req.body.confirm_password;
     const register = await registerUser;
 
-    if (email.length < 0) {
+
+    if (!email || !password || !confirmPW) {
+        req.flash('error', 'One or more fields left blank. Please fill all fields.')
         res.redirect('/register');
+    }
+
+    if (email.length < 1) {
+        res.redirect('/register');
+        req.flash('error','Email was left blank.');
         return;
     }
 
     if (password.length < 8) {
         res.redirect('/register');
+        req.flash('error', "Password isn't long enough.")
         return;
     }
 
     if (!confirmPW || password === confirmPW ) {
         res.redirect('/register');
+        req.flash('error', "Passwords don't match. Please try again.")
         return;
     }
+
+    register(email, password);
 });
 
 
@@ -40,9 +51,29 @@ router.post('/login', async(req, res) => {
     const email = req.body.email;
     const password = req.body.password;
     const verify = await verifyUser;
-})
+
+    if (!email || !password || !confirmPW) {
+        req.flash('error', 'One or more fields left blank. Please fill all fields.')
+        res.redirect('/register');
+    }
+
+    if (email.length < 1) {
+        res.redirect('/register');
+        req.flash('error','Email was left blank.');
+        return;
+    }
+
+    if (password.length < 8) {
+        res.redirect('/register');
+        req.flash('error', "Password isn't long enough.")
+        return;
+    }
+
+});
 
 
 router.get('/', forceToBeLoggedIn, (req, res) => {
     res.render('account/index', {title: 'Account'});
-})
+});
+
+export default router;
